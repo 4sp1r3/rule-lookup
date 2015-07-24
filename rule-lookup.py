90594c8e43e65c4d426607835c9c9e2b11b85e9a
yfzblu <adam.witt@yahoo.com>
2015-07-24 15:15:24 -0700

    Re-added support for multiple rules files

--- rule-lookup.py
+++ rule-lookup.py
@@ -7,52 +7,80 @@ import sys
 
 
 class RuleLookup(object):
+
     def __init__(self):
-        self.sensorhostname = ""
-        self.allrules_location = ""
+        self.sensor = ""
+        self.rulesfile_type = "single/multiple"
+        self.rules_location = "/"
+        self.sid = None
+
+        self.credentials = False
         self.password_protected_key = False
+        self.keypassword = False
+
         self.allrules_url = None
-        self.allrules = True
-        self.credentials = None
 
 
-    def command(self, auth_type, command):
+    def command(self, search_string):
+
+        if self.rulesfile_type == "single":
+            command = "grep '{}' {}".format(search_string, self.rules_location)
+
+        elif self.rulesfile_type == mutliple:
+            command = "for file in $(ls {})" \
+                      "do grep {} $file; done".format(self.rules_location, search_string)
+        else:
+            sys.exit("[-] Set the rulesfile_type variable.\nExiting..")
+
+        return command
+
+
+    def ssh_auth_password(self, command):
+
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        if not self.credentials:
+            self.username = raw_input("Username: ")
+            self.password = getpass.getpass()
+            self.credentials = True
+
+        try:
+            ssh.connect(
+                    self.sensor,
+                    allow_agent=False,
+                    look_for_keys=False,
+                    username=self.username,
+                    password=self.password,
+                       )
+
+        except paramiko.AuthenticationException:
+            sys.exit("[-] Authentication failed")
+
+        stdin, stdout, stderr = ssh.exec_command(command)
+
+        return stdout.read()
+
+
+    def ssh_auth_key(self, command):
 
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         
 
-        if auth_type == "certificate":
-            if not self.credentials:
-                self.username = raw_input("Username: ")
-                self.credentials = True
+        if not self.credentials:
+            self.username = raw_input("Username: ")
+            self.credentials = True
 
-            try:
-                ssh.connect(
-                        self.sensorhostname,
-                        username=self.username,
-                           )
+        try:
+            ssh.connect(
+                    self.sensor,
+                    username=self.username,
+                       )
 
-            except paramiko.AuthenticationException:
-                sys.exit("[-] Authentication failed")
+        except paramiko.AuthenticationException:
+            sys.exit("[-] Authentication failed")
 
-        if auth_type == "password":
-
-            if not self.credentials:
-                self.username = raw_input("Username: ")
-                self.password = getpass.getpass()
-                self.credentials = True
-
-            try:
-                ssh.connect(
-                        self.sensorhostname,
-                        allow_agent=False
-                        look_for_keys=False,
-                        username=self.username,
-                        password=self.password,
-                           )
-            except paramiko.AuthenticationException:
-                sys.exit("[-] Authentication failed")
 
         stdin, stdout, stderr = ssh.exec_command(command)
 
@@ -77,30 +105,22 @@ class RuleLookup(object):
                 self.flowbits.append(line)
 
 
-    def rulelookup_ssh(self, auth_type, sid):
+    def ssh_results(self, results, sid):
+        
+        if not results:
+            sys.exit("[-] sid not found")
 
-        findsid = "grep {} {}".format(sid, self.allrules_location)
-
-        foundrule = self.command(auth_type, findsid)
-
-        if not foundrule:
-            print "[-] sid not found"
-            sys.exit()
-
-        elif foundrule and ("flowbits:isset" in foundrule):
-            reg = re.search("flowbits:isset,([a-zA-Z0-9\.]+)", foundrule)
+        elif results and ("flowbits:isset" in results):
+            reg = re.search("flowbits:isset,([a-zA-Z0-9\.]+)", results)
             flowbitgroup = reg.group(1)
-            findflowbits = "grep \"flowbits:set,%s;\" %s" % (flowbitgroup, self.allrules_location)
-            foundrule = foundrule
-            foundbits = self.command(auth_type, findflowbits)
+            findbits_command = self.command("flowbits:set,%s;" % (flowbitgroup))
+            foundbits = self.ssh_auth_key(findbits_command)
 
-            print "\n==============\nsid {}\n==============\n\n{}".format(sid, foundrule)
+            print "\n==============\nsid {}\n==============\n\n{}".format(sid, results)
             print "==============\nFlowbits\n==============\n\n{}".format(foundbits)
 
         else:
             print "\n==============\nsid {}\n==============\n\n{}".format(sid, foundrule)
-
-        
 
 
 p = ArgumentParser()
@@ -110,12 +130,16 @@ p.add_argument("-c", "--certificate", help="SSH and certificate authentication",
 p.add_argument("sid", help="Rule sid")
 args = p.parse_args()
 
-if args.password:
+if args.certificate:
     r = RuleLookup()
-    r.rulelookup_ssh("password", args.sid)
+    command = r.command(args.sid)
+    results = r.ssh_auth_key(command)
+    r.ssh_results(results, args.sid)
 
-elif args.certificate:
+elif args.password:
     r = RuleLookup()
-    r.rulelookup_ssh("certificate", args.sid)
+    command = r.command(args.sid)
+    results = r.ssh_auth_password(command)
+    r.ssh_results(results, args.sid)
 else:
     print "[-] No arguments provided. Use -h for assistance"
