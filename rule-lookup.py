9722a68e0a9c51893d99989cdbbeef393a1959bb
Adam Witt <adam.witt@yahoo.com>
2015-07-26 17:51:00 -0700

    More work on multiple rules files..

--- rule-lookup.py
+++ rule-lookup.py
@@ -9,31 +9,41 @@ import sys
 class RuleLookup(object):
 
     def __init__(self):
+
+        # The two settings below are not to be touched by the User,
+        # they pseudo track state and authentication types to
+        # prevent Paramiko from prompting for authentication during 
+        # second connection to check for flowbits 
+        self.auth_type = None    
+        self.credentials = False
+
+        # Configuration settings, to be selected by the User
+        # Sensor can be IP or hostname
+        # rulesfile_type identifies a single rules file or multiple
+        # rules_location is either a full directory path containing-
+        # multiple rules files, or the full path to one rules file
         self.sensor = ""
         self.rulesfile_type = "single/multiple"
-        self.rules_location = "/"
-        self.sid = None
+        self.rules_location = ""
 
-        self.credentials = False
         self.password_protected_key = False
         self.keypassword = False
 
         self.allrules_url = None
-
 
     def command(self, search_string):
 
         if self.rulesfile_type == "single":
             command = "grep '{}' {}".format(search_string, self.rules_location)
 
-        elif self.rulesfile_type == mutliple:
-            command = "for file in $(ls {})" \
-                      "do grep {} $file; done".format(self.rules_location, search_string)
+        elif self.rulesfile_type == "multiple":
+            command = "for file in $(ls {0});" \
+            "do grep '{1}' {0}$file; done".format(self.rules_location, search_string)
+
         else:
             sys.exit("[-] Set the rulesfile_type variable.\nExiting..")
 
         return command
-
 
     def ssh_auth_password(self, command):
 
@@ -61,7 +71,6 @@ class RuleLookup(object):
 
         return stdout.read()
 
-
     def ssh_auth_key(self, command):
 
         ssh = paramiko.SSHClient()
@@ -86,7 +95,6 @@ class RuleLookup(object):
 
         return stdout.read()
 
-
     def rulelookup_html(self, sid):
         flowbits = []
 
@@ -104,42 +112,75 @@ class RuleLookup(object):
             if "flowbits:set,{};".format(flowbitgroup) in line:
                 self.flowbits.append(line)
 
+    def get_flowbits(self, results):
 
-    def ssh_results(self, results, sid):
-        
-        if not results:
-            sys.exit("[-] sid not found")
+        reg = re.search("flowbits:isset,([a-zA-Z0-9\.]+)", sidresults)
+        flowbitgroup = reg.group(1)
+        findbits_command = r.command("flowbits:set,%s;" % (flowbitgroup))
 
-        elif results and ("flowbits:isset" in results):
-            reg = re.search("flowbits:isset,([a-zA-Z0-9\.]+)", results)
-            flowbitgroup = reg.group(1)
-            findbits_command = self.command("flowbits:set,%s;" % (flowbitgroup))
-            foundbits = self.ssh_auth_key(findbits_command)
+        if self.auth_type == "key":
+            flowbits = self.ssh_auth_key(findbits_command)
 
-            print "\n==============\nsid {}\n==============\n\n{}".format(sid, results)
-            print "==============\nFlowbits\n==============\n\n{}".format(foundbits)
+        elif self.auth_type == "password":
+            flowbits = self.ssh_auth_password(findbits_command)
 
-        else:
-            print "\n==============\nsid {}\n==============\n\n{}".format(sid, foundrule)
+        return flowbits
 
 
 p = ArgumentParser()
 p.add_argument("-w", "--web", help="Get rule from an HTML page", action="store_true")
 p.add_argument("-p", "--password", help="SSH and password authentication", action="store_true")
-p.add_argument("-c", "--certificate", help="SSH and certificate authentication", action="store_true")
+p.add_argument("-k", "--key", help="SSH and key-based authentication", action="store_true")
 p.add_argument("sid", help="Rule sid")
 args = p.parse_args()
 
-if args.certificate:
+if args.key:
     r = RuleLookup()
-    command = r.command(args.sid)
-    results = r.ssh_auth_key(command)
-    r.ssh_results(results, args.sid)
+    r.auth_type = "key"
+    sidcommand = r.command("sid:" + args.sid)
+    sidresults = r.ssh_auth_key(sidcommand)
+
+    if sidresults:
+        if "flowbits:isset" in sidresults:
+            flowbits = r.get_flowbits(sidresults)
+    else:
+        print "[-] sid not found"
+
+    if flowbits:
+        print flowbits
+    
 
 elif args.password:
     r = RuleLookup()
-    command = r.command(args.sid)
-    results = r.ssh_auth_password(command)
-    r.ssh_results(results, args.sid)
+    r.auth_type = "password"
+    sidcommand = r.command("sid:" + args.sid)
+    sidresults = r.ssh_auth_password(sidcommand)
+
+    if not sidresults:
+        print "[-] sid not found"
+        sys.exit()
+
+    elif "flowbits:isset" in sidresults:
+        flowbits = r.get_flowbits(sidresults)
+
+    else:
+        print "============="
+        print "sid: {}".format(args.sid)
+        print "=============\n"
+        print sidresults
+
+
+
+
 else:
     print "[-] No arguments provided. Use -h for assistance"
+
+
+
+
+
+
+
+
+
+
